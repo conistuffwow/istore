ca87149fa2e7bdd288eaa567f350db84967900a1
Coni <coni@Conis-MacBook-Pro.local>
2025-07-12 14:00:00 -0400

    beeb

--- js/script.js
+++ js/script.js
@@ -1,152 +1,70 @@
-(function(){
-    function getiOSVersion() {
-      var match = navigator.userAgent.match(/OS (\d+)_/);
-      return match ? parseInt(match[1], 10) : null;
-    }
-    function checkCydiaScheme(callback) {
-      var iframe = document.createElement('iframe');
-      iframe.style.display = 'none';
-      document.body.appendChild(iframe);
-      var timeout = setTimeout(function() {
-        document.body.removeChild(iframe);
-        callback(false);
-      }, 1000);
-      window.onblur = function() {
-        clearTimeout(timeout);
-        document.body.removeChild(iframe);
-        callback(true);
-      };
-      iframe.src = 'cydia://package/com.example.test';
-    }
-    function getQueryParam(name) {
-      var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(window.location.href);
-      return results ? decodeURIComponent(results[1]) : null;
-    }
-    function showJailbreakWarning() {
-      var jbWarn = document.getElementById('jailbreak-warning');
-      var stdUI = document.getElementById('standard-install-ui');
-      if(jbWarn) jbWarn.style.display = 'block';
-      if(stdUI) stdUI.style.display = 'none';
-    }
-    function showStandardInstall() {
-      var jbWarn = document.getElementById('jailbreak-warning');
-      var stdUI = document.getElementById('standard-install-ui');
-      if(jbWarn) jbWarn.style.display = 'none';
-      if(stdUI) stdUI.style.display = 'block';
-    }
-    function fixInstallLinks() {
-      var iosVersion = getiOSVersion();
-      if(iosVersion >= 7) {
-        var links = document.getElementsByTagName('a');
-        for(var i=0;i<links.length;i++) {
-          var href = links[i].getAttribute('href');
-          if(href && href.indexOf('itms-services://')===0 && href.indexOf('url=http://')!==-1) {
-            var fixedHref = href.replace('url=http://','url=https://');
-            links[i].setAttribute('href', fixedHref);
-          }
-        }
-      }
-    }
-    function loadApps() {
-      var selectedCategory = getQueryParam('category') || 'All';
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'content.xml', true);
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-          var parser, xml;
-          if(!xhr.responseXML || !xhr.responseXML.documentElement) {
-            if(window.DOMParser) {
-              parser = new DOMParser();
-              xml = parser.parseFromString(xhr.responseText, 'application/xml');
-            } else {
-              xml = new ActiveXObject('Microsoft.XMLDOM');
-              xml.async = false;
-              xml.loadXML(xhr.responseText);
-            }
-          } else {
-            xml = xhr.responseXML;
-          }
-          var apps = xml.getElementsByTagName('app');
-          var container = document.getElementById('app-list');
-          var categoryBar = document.getElementById('category-bar');
-          if(!container || !categoryBar) return;
-          container.innerHTML = '';
-          categoryBar.innerHTML = '';
-          var categories = {'All': true};
-          for(var i=0;i<apps.length;i++) {
-            var catNode = apps[i].getElementsByTagName('category')[0];
-            if(catNode) {
-              var cat = catNode.textContent || catNode.text;
-              categories[cat] = true;
-            }
-          }
-          var currentPage = window.location.pathname.split('/').pop();
-          var select = document.createElement('select');
-          select.id = 'category-select';
-          for(var c in categories) {
-            if(categories.hasOwnProperty(c)) {
-              var option = document.createElement('option');
-              option.value = c;
-              option.text = c;
-              select.appendChild(option);
-            }
-          }
-          categoryBar.appendChild(select);
-          for(var s=0;s<select.options.length;s++) {
-            if(select.options[s].value === selectedCategory) {
-              select.selectedIndex = s;
-              break;
-            }
-          }
-          select.onchange = function() {
-            window.location.href = currentPage + '?category=' + encodeURIComponent(this.value);
-          };
-          for(var j=0;j<apps.length;j++) {
-            var app = apps[j];
-            var titleNode = app.getElementsByTagName('title')[0];
-            var versionNode = app.getElementsByTagName('version')[0];
-            var iconNode = app.getElementsByTagName('icon')[0];
-            var plistNode = app.getElementsByTagName('plist')[0];
-            var categoryNode = app.getElementsByTagName('category')[0];
-            var minVersionNode = app.getElementsByTagName('min-version')[0];
-            if(!titleNode || !versionNode || !iconNode || !plistNode || !categoryNode || !minVersionNode) continue;
-            var title = titleNode.textContent || titleNode.text;
-            var version = versionNode.textContent || versionNode.text;
-            var icon = iconNode.textContent || iconNode.text;
-            var plist = plistNode.textContent || plistNode.text;
-            var category = categoryNode.textContent || categoryNode.text;
-            var minVersion = minVersionNode.textContent || minVersionNode.text;
-            if(selectedCategory !== 'All' && category !== selectedCategory) continue;
-            var div = document.createElement('div');
-            div.className = 'app';
-            div.innerHTML =
-              '<img src="' + icon + '" alt="' + title + ' Icon" class="icon">' +
-              '<div class="details">' +
-              '<h2>' + title + '</h2>' +
-              '<p>Version ' + version + '</p>' +
-              '<p>Requires iOS ' + minVersion + '+</p>' +
-              '<a class="install-button" href="itms-services://?action=download-manifest&url=' + encodeURIComponent(plist) + '">Install</a>' +
-              '</div>';
-            container.appendChild(div);
-          }
-          fixInstallLinks();
-        }
-      };
-      xhr.send();
-    }
-    var iosVersion = getiOSVersion();
-    if(iosVersion >=7) {
-      checkCydiaScheme(function(isJailbroken) {
-        if(isJailbroken) {
-          showJailbreakWarning();
-        } else {
-          showStandardInstall();
-          loadApps();
-        }
+(function () {
+  var ua = navigator.userAgent;
+  var isiOS = /iPhone|iPad|iPod/i.test(ua);
+  var iosVersion = isiOS ? parseFloat((ua.match(/OS (\d+_\d+)/) || [])[1]?.replace('_', '.') || 0) : 0;
+  var isModern = iosVersion >= 7;
+  var fileURLModifier = isModern ? (url => url.replace('http://', 'https://')) : (url => url);
+
+  var urlParams = new URLSearchParams(window.location.search);
+  var filterCategory = urlParams.get('category');
+
+  fetch('content.xml')
+    .then(res => res.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, 'text/xml'))
+    .then(data => {
+      var apps = Array.from(data.getElementsByTagName('app'));
+      var container = document.getElementById('app-list');
+      var categories = new Set();
+
+      apps.forEach(app => {
+        var title = app.getElementsByTagName('title')[0].textContent;
+        var version = app.getElementsByTagName('version')[0].textContent;
+        var icon = app.getElementsByTagName('icon')[0].textContent;
+        var ipa = app.getElementsByTagName('ipa')[0].textContent;
+        var category = app.getElementsByTagName('category')[0]?.textContent || 'Other';
+        var minVersion = parseFloat(app.getElementsByTagName('min-version')[0]?.textContent || 0);
+
+        categories.add(category);
+
+        if (iosVersion && iosVersion < minVersion) return;
+        if (filterCategory && category !== filterCategory) return;
+
+        var el = document.createElement('div');
+        el.className = 'app';
+        el.innerHTML = `
+          <img class="icon" src="${fileURLModifier(icon)}" />
+          <h2>${title}</h2>
+          <p>Version ${version}</p>
+          <a class="install-button" href="${fileURLModifier(ipa)}">Download IPA</a>
+        `;
+        container.appendChild(el);
       });
-    } else {
-      showStandardInstall();
-      loadApps();
-    }
-  })();
-  
+
+      var catBar = document.getElementById('category-bar');
+      if (catBar) {
+        var select = document.createElement('select');
+        select.id = 'category-select';
+
+        var allOption = document.createElement('option');
+        allOption.text = 'All Categories';
+        allOption.value = '';
+        select.appendChild(allOption);
+
+        Array.from(categories).sort().forEach(cat => {
+          var opt = document.createElement('option');
+          opt.value = cat;
+          opt.text = cat;
+          select.appendChild(opt);
+        });
+
+        select.addEventListener('change', function () {
+          var base = location.pathname.includes('7.html') ? '7.html' : 'index.html';
+          var param = this.value ? '?category=' + encodeURIComponent(this.value) : '';
+          window.location.href = base + param;
+        });
+
+        catBar.appendChild(select);
+        if (filterCategory) select.value = filterCategory;
+      }
+    });
+})();
